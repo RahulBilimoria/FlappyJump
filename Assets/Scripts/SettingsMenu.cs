71d38e37b2ca6e23fcc22fa505ee3a4a675b9e15
RahulBilimoria <rahulbilimoria1@gmail.com>
2018-12-23 19:35:36 -0500

    almost done settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AudioMixer audioMixer;
+
+	public void SetVolume(float value) {
+        audioMixer.SetFloat("Volume", value);
+    }
+
+    public void SetQuality(int index) {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullscreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
 }
