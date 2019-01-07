7571d6cb7b9f8bd500e0a9dd650b1fe2d2246734
RahulBilimoria <rahulbilimoria1@gmail.com>
2019-01-07 18:06:57 -0500

    added horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float fallModifier = 2.5f;
-    public float jumpSpeed = 3f;
+    //change to public later
+    float movementSpeed = 5f;
+    float maxMovementSpeed = 3f;
+    float jumpSpeed = 6f;
+    float fallModifier = 2.5f;
 
     float horizontalMove = 0f;
 
-    float yPos = 5f;
-    float xPos = 0f;
-
-    int score;
-
-    bool grounded = false;
+    bool canJump = false;
     bool doubleJump = true;
     bool isJumping = false;
     
@@ -25,20 +23,14 @@ public class PlayerController : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Use this for initialization
-    void Start () {
-        //movementVector = GetComponent<Rigidbody2D>();
-	}
-
     void Update() {
 
-        if (rb.velocity.y < 0) {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallModifier - 1) * Time.deltaTime;
-        }
+        //gets left & right input
+        horizontalMove = Input.GetAxisRaw("Horizontal");
 
-        //horizontalMove = Input.GetAxisRaw("Horizontal");
+        //gets jump imput
         if (Input.GetKeyDown(KeyCode.Space)) {
-            if (grounded || doubleJump) {
+            if (canJump || doubleJump) {
                 isJumping = true;
             } if (doubleJump) {
                 doubleJump = false;
@@ -48,38 +40,49 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-        //Vector2 move = Vector2.zero;
-        //move.x = Input.GetAxis("Horizontal");
-        
+        //changes gravity to make player fall faster
+        if (rb.velocity.y < 0) {
+            rb.gravityScale = fallModifier;
+        } else {
+            rb.gravityScale = 1f;
+        }
+
+        //adds force for jumping
         if (isJumping) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            // if player is falling, adds a force equal to its falling speed to simulate a jump in mid air
+            Vector2 jumpForce = rb.velocity.y < 0 ? Vector2.up * (rb.velocity.y * -1 + jumpSpeed) : Vector2.up * jumpSpeed;
+
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
             isJumping = false;
         }
 
-		/*if (Input.GetKeyDown(KeyCode.W))
-        {
-            jumpVector.velocity = new Vector2(xPos, yPos);
+        //adds force for left and right movement
+        if (horizontalMove < 0) {
+            // if player changes directions, adds a force to "cancel" out its current velocity
+            if (rb.velocity.x > 0) rb.AddForce(rb.velocity.x * Vector2.right * -1, ForceMode2D.Impulse);
+
+            rb.AddForce(Vector2.right * -1 * movementSpeed, ForceMode2D.Force);
+        } else if (horizontalMove > 0) {
+            // if player changes directions, adds a force to "cancel" out its current velocity
+            if (rb.velocity.x < 0) rb.AddForce(rb.velocity.x * Vector2.right * -1, ForceMode2D.Impulse);
+
+            rb.AddForce(Vector2.right * movementSpeed, ForceMode2D.Force);
+        } else if (horizontalMove == 0 && rb.velocity.x != 0) {
+            // if there is no input, adds a force to gradually slow down the player
+            rb.AddForce(rb.velocity.x * Vector2.right * -0.05f, ForceMode2D.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            jumpVector.velocity = new Vector2(xPos + 1, yPos);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            jumpVector.velocity = new Vector2(xPos - 1, yPos);
-        }*/
 
 	}
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Floor") {
-            grounded = true;
+            canJump = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision) {
         if (collision.gameObject.tag == "Floor") {
-            grounded = false;
+            canJump = false;
             doubleJump = true;
         }
     }
