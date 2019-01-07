1e62fe762b975f174a4d2c76e0914fb3ee1f09d7
RahulBilimoria <rahulbilimoria1@gmail.com>
2019-01-06 19:50:34 -0500

    added faster fall speed for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float fallModifier = 2.5f;
+    public float jumpSpeed = 3f;
+
     float horizontalMove = 0f;
-    float jumpSpeed = 3f;
+
+    float yPos = 5f;
+    float xPos = 0f;
 
     int score;
 
     bool grounded = false;
     bool doubleJump = true;
     bool isJumping = false;
-    float yPos = 5f;
-    float xPos = 0f;
-    Rigidbody2D movementVector;
+    
+    Rigidbody2D rb;
     Vector2 velocity = new Vector2(0,0);
 
-	// Use this for initialization
-	void Start () {
-        movementVector = GetComponent<Rigidbody2D>();
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        //movementVector = GetComponent<Rigidbody2D>();
 	}
 
     void Update() {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallModifier - 1) * Time.deltaTime;
+        }
+
+        //horizontalMove = Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (grounded || doubleJump) {
                 isJumping = true;
@@ -35,17 +48,13 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-        Vector2 move = Vector2.zero;
-        move.x = Input.GetAxis("Horizontal");
-
-        velocity = movementVector.velocity;
+        //Vector2 move = Vector2.zero;
+        //move.x = Input.GetAxis("Horizontal");
         
         if (isJumping) {
-            velocity.y = jumpSpeed;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             isJumping = false;
         }
-
-        movementVector.velocity = new Vector2(velocity.x, velocity.y);
 
 		/*if (Input.GetKeyDown(KeyCode.W))
         {
