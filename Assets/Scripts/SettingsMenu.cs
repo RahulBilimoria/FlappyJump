015d3ec7ba1ff4c86db37b7935643440816aa13e
RahulBilimoria <rahulbilimoria1@gmail.com>
2019-02-09 15:10:30 -0500

    updated menus

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -7,32 +7,6 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour {
 
     public AudioMixer audioMixer;
-    public Dropdown resolutionDropdown;
-    Resolution[] resolutions;
-
-    public void Start() {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++) {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
 
     public void SetVolume(float value) {
         audioMixer.SetFloat("Volume", value);
@@ -44,10 +18,5 @@ public class SettingsMenu : MonoBehaviour {
 
     public void SetFullscreen(bool isFullscreen) {
         Screen.fullScreen = isFullscreen;
-    }
-
-    public void SetResolution(int resolutionIndex) {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
